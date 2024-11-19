ca52c39a93f98db9e1d638554719b5d315d14393
benjamibono <benjamibono@gmail.com>
2024-11-19 12:54:31 +0100

    start build

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+import animations from "@midudev/tailwind-animations";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    screens: {
+      mobile: "360px",
+      tablet: "640px",
+      md: "768px",
+      laptop: "1024px",
+      pc: "1280px",
+    },
+  },
+  plugins: [animations],
+};
