f624145cc8231b0f9d279c64a0aaec0e0629aea6
benjamibono <benjamibono@gmail.com>
2024-12-18 18:30:59 +0100

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,25 @@
 import animations from "@midudev/tailwind-animations";
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
+      },
       overscrollBehavior: {
         none: "none",
-      }
+      },
     },
     screens: {
-      mobile: "360px",
-      tablet: "640px",
+      xs: "360px",
+      sm: "640px",
       md: "768px",
-      laptop: "1024px",
-      pc: "1280px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px"
     },
   },
   plugins: [animations],
